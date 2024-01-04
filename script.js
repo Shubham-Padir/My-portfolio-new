79ff622aabac2b28a1a7f78f09704e719538d4f0
Shubham Padir <shubhamsunilpadirsp2003@gmail.com>
2024-01-04 09:28:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+let nav = document.querySelector("#nav");
+ let sticky = nav.offsetTop;
+
+ window.addEventListener('scroll', ()=>{
+  if(window.scrollY > sticky){
+    nav.classList.add('sticky');
+  }else{
+    nav.classList.remove('sticky');
+  }
+ });
+
+ let menu = document.querySelector(".navright i")
+ let full = document.querySelector("#full-scr")
+ let flag = 0;
+ menu.addEventListener("click", function(){
+  if(flag==0){
+    full.style.top = 0
+    flag = 1;
+  }else{
+    full.style.top = "-100%"
+    flag = 0;
+  }
+    
+ })
+ 
+//  let full1 = document.querySelector("#full-scr")
+//  let fulldiv = document.querySelector(".full-div1")
+//  let fulldivA = document.querySelector(".full-div1 a")
+//  let flag1 = 0
+//  fulldivA.addEventListener("click", function(){
+//    // fulldiv.style.display = "none"
+//    full1.style.display = "none"
+
+//   if(flag1==0){
+//     fulldiv.style.top = 0
+//     flag1 = 1;
+//   }else{
+//     fulldiv.style.top = "-100%"
+//     flag1 = 0;
+//   }
+   
+//  })
